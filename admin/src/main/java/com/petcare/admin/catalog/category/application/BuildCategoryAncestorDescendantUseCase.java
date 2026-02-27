655a37d2941b0174c8e4020a7a5d9bb8b6ea57a3
abdullah <abdullah.ramdan01@gmail.com>
2026-02-27 02:13:18 +0200

    applying batching while building categoryAncestorDescendant

--- admin/src/main/java/com/petcare/admin/catalog/category/application/BuildCategoryAncestorDescendantUseCase.java
+++ admin/src/main/java/com/petcare/admin/catalog/category/application/BuildCategoryAncestorDescendantUseCase.java
@@ -1,37 +1,43 @@
 package com.petcare.admin.catalog.category.application;
 
-import com.petcare.admin.catalog.category.domain.CategoryAncestorDescendant;
 import com.petcare.admin.catalog.category.repository.CategoryAncestorDescendantRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 @AllArgsConstructor
 public class BuildCategoryAncestorDescendantUseCase {
 
-  private final CategoryAncestorDescendantRepository categoryAncestorDescendantRepository;
+  private final CategoryAncestorDescendantRepository ancestorDescendantRepository;
+  private final JdbcTemplate jdbcTemplate;
+
+  record AncestorDescendant(long ancestorId, long descendantId, int depth) {}
 
   public void execute() {
+    log.info("Starting building categoryAncestorDescendant");
 
     // Build data FIRST, delete/insert LAST. This minimizes lock duration.
-    List<CategoryAncestorDescendant> allRows = buildAllRows();
+    List<AncestorDescendant> allRows = buildAllRows();
 
     // Locking starts here
-    categoryAncestorDescendantRepository.deleteAll();
+    ancestorDescendantRepository.deleteAll();
 
-    // Step 4: allRows insert
-    // TODO: Using batching is better
-    categoryAncestorDescendantRepository.saveAll(allRows);
+    // Step 4: allRows insert (using batch)
+    batchUpdate(allRows);
+    log.info("Starting building categoryAncestorDescendant");
   }
 
-  private List<CategoryAncestorDescendant> buildAllRows() {
-    List<Object[]> rows = categoryAncestorDescendantRepository.findCategoriesIdsAndParentsIds();
+  private List<AncestorDescendant> buildAllRows() {
+    List<Object[]> rows = ancestorDescendantRepository.findCategoriesIdsAndParentsIds();
     Map<Long, Long> descendantIdToAncestorIdMap = new HashMap<>(rows.size());
 
     for (Object[] row : rows) {
@@ -41,17 +47,29 @@ public class BuildCategoryAncestorDescendantUseCase {
     }
 
     // Step 3: build closure relations
-    List<CategoryAncestorDescendant> allRows = new ArrayList<>(rows.size() * 3);
+    List<AncestorDescendant> allRows = new ArrayList<>(rows.size() * 3);
 
     for (Long descendantId : descendantIdToAncestorIdMap.keySet()) {
       Long ancestorId = descendantId;
       int depth = 0;
       while (ancestorId != null) {
-        allRows.add(CategoryAncestorDescendant.of(ancestorId, descendantId, depth++));
+        allRows.add(new AncestorDescendant(ancestorId, descendantId, depth++));
         ancestorId = descendantIdToAncestorIdMap.get(ancestorId);
       }
     }
 
     return allRows;
+  }
+
+  private void batchUpdate(List<AncestorDescendant> allRows) {
+    jdbcTemplate.batchUpdate(
+        "insert into category_ancestor_descendant (ancestor_id, descendant_id, depth) values( ?, ?, ?)",
+        allRows,
+        200,
+        (ps, entry) -> {
+          ps.setLong(1, entry.ancestorId);
+          ps.setLong(2, entry.descendantId);
+          ps.setInt(3, entry.depth);
+        });
   }
 }
